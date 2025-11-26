de49b2c9a60354278aa5b7f8dc48d39302a58391
veqox <veqoxao@gmail.com>
2025-11-26 18:07:45 +0100

    feat: use macros to remove boilerplate

--- crates/ble/src/hci/command.rs
+++ crates/ble/src/hci/command.rs
@@ -1,4 +1,5 @@
 use crate::hci::{BufWriter, WriteHCI, gap::AdvData};
+use ble_macros::command;
 use embedded_io::Write;
 
 // const OGF_LINK_CONTROL_COMMAND: u16 = 0x01;
@@ -28,24 +29,10 @@ pub trait Command {
     fn write_payload<W: Write>(&self, w: &mut W) -> Result<(), W::Error>;
 }
 
+#[command(ogf = OGF_CONTROL_AND_BASEBAND_COMMAND, ocf = 0x03)]
 pub struct Reset {}
 
-impl Command for Reset {
-    const OGF: u16 = OGF_CONTROL_AND_BASEBAND_COMMAND;
-    const OCF: u16 = 0x03;
-    const PARAM_LEN: u8 = 0;
-
-    fn write_payload<W: Write>(&self, _w: &mut W) -> Result<(), W::Error> {
-        Ok(())
-    }
-}
-
-impl Reset {
-    pub fn new() -> Self {
-        Self {}
-    }
-}
-
+#[command(ogf = OGF_LE_CONTROLLER_COMMAND, ocf = 0x06)]
 pub struct SetAdvParameters {
     pub interval_min: u16,
     pub interval_max: u16,
@@ -55,49 +42,6 @@ pub struct SetAdvParameters {
     pub peer_address: [u8; 6],
     pub advertising_channel_map: u8,
     pub advertising_filter_policy: u8,
-}
-
-impl SetAdvParameters {
-    pub fn new(
-        interval_min: u16,
-        interval_max: u16,
-        advertising_type: u8,
-        own_address_type: u8,
-        peer_address_type: u8,
-        peer_address: [u8; 6],
-        advertising_channel_map: u8,
-        advertising_filter_policy: u8,
-    ) -> Self {
-        Self {
-            interval_min,
-            interval_max,
-            advertising_type,
-            own_address_type,
-            peer_address_type,
-            peer_address,
-            advertising_channel_map,
-            advertising_filter_policy,
-        }
-    }
-}
-
-impl Command for SetAdvParameters {
-    const OGF: u16 = OGF_LE_CONTROLLER_COMMAND;
-    const OCF: u16 = 0x06;
-    const PARAM_LEN: u8 = 2 + 2 + 1 + 1 + 1 + 6 + 1 + 1;
-
-    fn write_payload<W: Write>(&self, w: &mut W) -> Result<(), W::Error> {
-        self.interval_min.write_into(w)?;
-        self.interval_max.write_into(w)?;
-        self.advertising_type.write_into(w)?;
-        self.own_address_type.write_into(w)?;
-        self.peer_address_type.write_into(w)?;
-        self.peer_address.as_slice().write_into(w)?;
-        self.advertising_channel_map.write_into(w)?;
-        self.advertising_filter_policy.write_into(w)?;
-
-        Ok(())
-    }
 }
 
 pub struct SetAdvData<'a> {
@@ -160,24 +104,7 @@ impl<'a> Command for SetScanResponseData<'_> {
     }
 }
 
+#[command(ogf = OGF_LE_CONTROLLER_COMMAND, ocf = 0x0A)]
 pub struct SetAdvEnable {
     pub enable: u8,
-}
-
-impl SetAdvEnable {
-    pub fn new(enable: u8) -> Self {
-        Self { enable }
-    }
-}
-
-impl Command for SetAdvEnable {
-    const OGF: u16 = OGF_LE_CONTROLLER_COMMAND;
-    const OCF: u16 = 0x0A;
-    const PARAM_LEN: u8 = 1;
-
-    fn write_payload<W: Write>(&self, w: &mut W) -> Result<(), W::Error> {
-        self.enable.write_into(w)?;
-
-        Ok(())
-    }
 }
